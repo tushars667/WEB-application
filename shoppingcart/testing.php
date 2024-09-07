c928ae0d11849034560114a3ee911e7d4ffd1949
tushars667 <124745290+tushars667@users.noreply.github.com>
2024-09-07 10:18:16 +0530

    Add files via upload

---
+++ shoppingcart/testing.php
@@ -0,0 +1,12 @@
+<?php
+try{
+    $dbh = new pdo( 'localhost',
+                    'root',
+                    '',
+                    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    die(json_encode(array('outcome' => true)));
+}
+catch(PDOException $ex){
+    die(json_encode(array('outcome' => false, 'message' => 'Unable to connect')));
+}
+?>
